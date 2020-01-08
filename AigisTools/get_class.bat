e599bf34be45314e4c743a08f0c0fac4c7049a67
K.T.Mirror <z1022001@gmail.com>
2020-01-08 21:56:21 +0800

    aigistools XML update

--- AigisTools/get_class.bat
+++ AigisTools/get_class.bat
@@ -3,4 +3,4 @@ SET PATH=%~dp0Utilities\Lua 5.3;%~dp0Utilities\cURL\bin;%~dp0Utilities\GraphicsM
 SET LUA_PATH=%~dp0Scripts\?.lua
 SET LUA_PATH_5_3=%~dp0Scripts\?.lua
 
-get_file.lua PlayerUnitTable.aar
+do get_file.lua PlayerUnitTable.aar
