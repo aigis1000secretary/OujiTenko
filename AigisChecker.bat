49fc0404ae8028c20cfbdf61d49cf19f6291b34a
K.T.Mirror <z1022001@gmail.com>
2020-05-15 00:11:40 +0800

    bat

--- AigisChecker.bat
+++ AigisChecker.bat
@@ -1,20 +1,24 @@
 
 @cd ./LocalProxy
-@echo set proxy 127.0.0.1:8000
-@echo login aigis
+@echo set proxy 127.0.0.1:8000 and login Aigis
 @node index.js
 
 @pause
 
+@echo moveing data...
 @start AigisTools.bat
-
-@cd ../AigisTools
 @pause
+
+@echo do get_icondata.bat
+@cd ../AigisTools
 @start get_icondata.bat
 @pause
+
+@echo moveing data...
 @start AigisChecker.bat
 @pause
 
+@echo making checker data
 @cd ../AigisChecker
 @node index.js
 
